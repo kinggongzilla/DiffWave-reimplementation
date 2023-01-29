7be4a73fd8c67e7a0bdd4a1d3415dc54bc5bbd88
David Hauser <hauser-david@hotmail.com>
2023-01-29 01:12:51 +0100

    batch_size=4; num_blocks=4; res_channels=32;

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ chunked_data = ChunkedMusDBHQ(audio_dir=data_path)
 EPOCHS = 5
 BATCH_SIZE = 4
 LEARNING_RATE = 2 * 1e-4
-NUM_BLOCKS = 2
-RES_CHANNELS = 128
+NUM_BLOCKS = 4
+RES_CHANNELS = 32
 TIME_STEPS = 50
 VARIANCE_SCHEDULE = torch.linspace(0.001, 0.05, TIME_STEPS)
 
