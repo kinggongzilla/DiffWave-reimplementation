1365af9112dd5d8cae0e2dc2bd0b26e7e9ddd0ac
David Hauser <hauser-david@hotmail.com>
2023-01-29 02:32:26 +0100

    improve length function of dataset

--- source/dataset.py
+++ source/dataset.py
@@ -12,14 +12,17 @@ class ChunkedMusDBHQ(Dataset):
     def __init__(self, audio_dir) -> None:
         self.audio_dir = audio_dir
 
-    def __len__(self):
         count = 0
         # Iterate directory
         for path in os.listdir(self.audio_dir):
             # check if current path is a file
             if os.path.isfile(os.path.join(self.audio_dir, path)):
                 count += 1
-        return count
+        self.length = count
+
+
+    def __len__(self):
+        return self.length
 
     def __getitem__(self, index):
         file = os.listdir(self.audio_dir)[index]
