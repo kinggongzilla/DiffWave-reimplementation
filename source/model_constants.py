bc7b609e6544fefba10779de7f652206dc7f5761
David Hauser <hauser-david@hotmail.com>
2023-02-01 14:49:51 +0100

    add comments for params used in DiffWave paper for unconditional training

--- source/model_constants.py
+++ source/model_constants.py
@@ -1,10 +1,11 @@
 import torch
 
+# params used in DiffWave paper for unconditional training in comments below
 EPOCHS = 5
-BATCH_SIZE = 4
+BATCH_SIZE = 4 #16
 LEARNING_RATE = 2 * 1e-4
-NUM_BLOCKS = 5
-RES_CHANNELS = 32
-TIME_STEPS = 50
-VARIANCE_SCHEDULE = torch.linspace(0.001, 0.05, TIME_STEPS)
-LAYER_WIDTH = 128
+NUM_BLOCKS = 5 #36
+RES_CHANNELS = 32 #256
+TIME_STEPS = 50 #200
+VARIANCE_SCHEDULE = torch.linspace(10e-4, 0.02, TIME_STEPS)
+LAYER_WIDTH = 128 #512
