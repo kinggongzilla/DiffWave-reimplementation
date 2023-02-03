a64ac696f12eb5e1437bf01f6a109ffbf9c59a3b
David Hauser <hauser-david@hotmail.com>
2023-02-03 21:23:44 +0100

    add model path as parameter for sample.py

--- sample.py
+++ sample.py
@@ -1,17 +1,22 @@
 import os
+import sys
 import torch
 import torchaudio
 from source.model import DiffWave
-from source.model_constants import NUM_BLOCKS, RES_CHANNELS, TIME_STEPS, VARIANCE_SCHEDULE, LAYER_WIDTH
+from source.model_constants import NUM_BLOCKS, RES_CHANNELS, TIME_STEPS, VARIANCE_SCHEDULE, LAYER_WIDTH, SAMPLE_RATE
 
+model_path = "outputs/models/model.pt"
 
+#get first commandline argument
+if len(sys.argv) > 1:
+    model_path = sys.argv[1]
 
 model = DiffWave(RES_CHANNELS, NUM_BLOCKS, TIME_STEPS, VARIANCE_SCHEDULE, LAYER_WIDTH)
-model.load_state_dict(torch.load('outputs/models/model.pt'))
+model.load_state_dict(torch.load(model_path))
 model.eval()
 
-noise = torch.randn(1, 1, 22050*5) # 22KHz * 5000 milliseconds = 5 seconds of noise
+noise = torch.randn(1, 1, SAMPLE_RATE*5) # 22KHz * 5000 milliseconds = 5 seconds of noise
 y = model.sample(noise)
 for i in range(y.shape[0]): #for each sample in batch
     path = os.path.join("outputs/samples", f"sample{i}.wav")
-    torchaudio.save(path, y[i], 44100//2)
+    torchaudio.save(path, y[i], SAMPLE_RATE)
