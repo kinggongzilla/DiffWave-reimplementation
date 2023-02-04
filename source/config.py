1bfac1a489cca3fa7163098d84c24927da8b20fe
David Hauser <hauser-david@hotmail.com>
2023-02-04 11:52:00 +0100

    update config to train on single sine audio sample

--- source/config.py
+++ source/config.py
@@ -1,9 +1,9 @@
 import torch
 
 # params used in DiffWave paper for unconditional training in comments below
-EPOCHS = 5
-BATCH_SIZE = 8 #16
-LEARNING_RATE = 1e-3 #2 * 1e-4
+EPOCHS = 5000
+BATCH_SIZE = 1 #16
+LEARNING_RATE = 2 * 1e-4
 NUM_BLOCKS = 8 #36
 RES_CHANNELS = 48 #256
 LAYER_WIDTH = 64 #512
@@ -14,7 +14,7 @@ SAMPLE_LENGTH_SECONDS = 5
 MAX_SAMPLES = 10000 # None for all samples in data input folder
 
 
-# local config for laptop
+# local config for laptop for debugging locally
 # EPOCHS = 5
 # BATCH_SIZE = 4 #16
 # LEARNING_RATE = 1e-3 #2 * 1e-4
