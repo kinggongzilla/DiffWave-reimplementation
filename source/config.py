c8948e7b1341df703510cc2a7645a680869569c8
David Hauser <hauser-david@hotmail.com>
2023-02-07 21:18:41 +0100

    update config to sample rate 8000 and bigger model

--- source/config.py
+++ source/config.py
@@ -2,16 +2,16 @@ import torch
 
 # params used in DiffWave paper for unconditional training in comments below
 EPOCHS = 15
-BATCH_SIZE = 16 #16
+BATCH_SIZE = 8 #16
 LEARNING_RATE = 2 * 1e-4
 NUM_BLOCKS = 9 #36
 RES_CHANNELS = 64 #256
 LAYER_WIDTH = 128 #512
 TIME_STEPS = 50 #200
 VARIANCE_SCHEDULE = torch.linspace(10e-4, 0.05, TIME_STEPS) #torch.linspace(10e-4, 0.05, TIME_STEPS)
-SAMPLE_RATE = 16000 #44100 #22050
-SAMPLE_LENGTH_SECONDS = 1
-MAX_SAMPLES = 3240 # None for all samples in data input folder
+SAMPLE_RATE = 8000 #16000 #44100 #22050
+SAMPLE_LENGTH_SECONDS = 4
+MAX_SAMPLES = 900 # None for all samples in data input folder
 
 
 # local config for laptop for debugging locally
