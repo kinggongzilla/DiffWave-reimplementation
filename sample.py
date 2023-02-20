18626f2f74312be0628b78e743770d4ad0d35767
David Hauser <hauser-david@hotmail.com>
2023-02-20 17:30:54 +0100

    add conditioner for sampling

--- sample.py
+++ sample.py
@@ -8,16 +8,23 @@ from source.config import NUM_BLOCKS, RES_CHANNELS, TIME_STEPS, VARIANCE_SCHEDUL
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model_path = "output/models/model.pt"
+conditioner_path = os.listdir("data/mel_spectrograms/")[0] #first file in mel_spectrogram folder
 if len(sys.argv) > 1:
     model_path = sys.argv[1]
+
+if len(sys.argv) > 2:
+    conditioner_path = sys.argv[2]
+
 
 
 model = DiffWave(RES_CHANNELS, NUM_BLOCKS, TIME_STEPS, VARIANCE_SCHEDULE, TIMESTEP_LAYER_WIDTH)
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.eval()
 
+#load conditioner
+conditioner = torch.load(os.path.join("data/mel_spectrograms", conditioner_path))
 noise = torch.randn(1, 1, SAMPLE_RATE*SAMPLE_LENGTH_SECONDS) # batch_size, n_channels, sample length e.g. 22,05KHz * 5000 milliseconds = 5 seconds of noise
-y = model.sample(noise)
+y = model.sample(noise, conditional=conditioner if model.with_conditioner else None)
 for i in range(y.shape[0]): #for each sample in batch
     path = os.path.join("output/samples", f"sample{i}.wav")
     torchaudio.save(path, y[i], SAMPLE_RATE)
