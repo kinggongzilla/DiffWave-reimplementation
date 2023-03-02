3b835254ee95a273f9f4b9c11998e4a0868009ba
David Hauser <hauser-david@hotmail.com>
2023-03-03 00:59:59 +0100

    update config

--- source/config.py
+++ source/config.py
@@ -1,6 +1,6 @@
 import torch
 
-EPOCHS = 3000
+EPOCHS = 100
 BATCH_SIZE = 6 #16
 LEARNING_RATE = 2 * 1e-4
 NUM_BLOCKS = 36 #36
@@ -10,7 +10,7 @@ TIME_STEPS = 200
 VARIANCE_SCHEDULE = torch.linspace(10e-4, 0.02, TIME_STEPS) #torch.linspace(10e-4, 0.05, TIME_STEPS)
 SAMPLE_RATE = 8000 #16000 #22050 #44100
 SAMPLE_LENGTH_SECONDS = 4
-MAX_SAMPLES = 1 #9000 # Use "None" for all samples in data input folder; 1000 = ~1h 6min
+MAX_SAMPLES = 9000 #9000 # Use "None" for all samples in data input folder; 1000 = ~1h 6min
 WITH_CONDITIONING=False
 
 #CONFIG DATA PREP TRANSFORM TO MEL SPECTROGRAM
