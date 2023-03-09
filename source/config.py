eee47ac2fd1a5b23e9e271e911618b72a3097977
David Hauser <hauser-david@hotmail.com>
2023-03-09 23:45:54 +0100

    update config

--- source/config.py
+++ source/config.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-EPOCHS = 1
+EPOCHS = 1000000
 BATCH_SIZE = 6 #16
 LEARNING_RATE = 2 * 1e-4 * np.sqrt(6/16) #downscale learning rate. by multiplying with sqrt(batch_size/original_batch_size) due to reduced batch size
 NUM_BLOCKS = 36 #36
@@ -11,7 +11,7 @@ TIME_STEPS = 200
 VARIANCE_SCHEDULE = torch.linspace(10e-4, 0.02, TIME_STEPS) #torch.linspace(10e-4, 0.05, TIME_STEPS)
 SAMPLE_RATE = 8000 #16000 #22050 #44100
 SAMPLE_LENGTH_SECONDS = 4
-MAX_SAMPLES = 1 #9000 # Use "None" for all samples in data input folder; 1000 = ~1h 6min
+MAX_SAMPLES = 9000 # Use "None" for all samples in data input folder; 1000 = ~1h 6min
 WITH_CONDITIONING=True
 
 #CONFIG DATA PREP TRANSFORM TO MEL SPECTROGRAM
