5a3a79de2945189bc9c640217a0b485b0ddafaef
David Hauser <hauser-david@hotmail.com>
2023-04-13 22:27:08 +0200

    fix variance schedule

--- source/config.py
+++ source/config.py
@@ -8,7 +8,7 @@ NUM_BLOCKS = 36 #36
 RES_CHANNELS = 64 #256
 TIMESTEP_LAYER_WIDTH = 512 #512
 TIME_STEPS = 200
-VARIANCE_SCHEDULE = torch.linspace(10e-4, 0.02, TIME_STEPS) #torch.linspace(10e-4, 0.05, TIME_STEPS)
+VARIANCE_SCHEDULE = torch.linspace(1e-4, 0.02, TIME_STEPS) #torch.linspace(10e-4, 0.05, TIME_STEPS)
 SAMPLE_RATE = 8000 #16000 #22050 #44100
 SAMPLE_LENGTH_SECONDS = 4
 MAX_SAMPLES = 9000 # Use "None" for all samples in data input folder; 1000 = ~1h 6min
