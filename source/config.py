ad97dce2de370d93f314d5ab079f23bc5d1f31b9
David Hauser <hauser-david@hotmail.com>
2023-04-20 01:03:40 +0200

    update config

--- source/config.py
+++ source/config.py
@@ -1,14 +1,14 @@
 import torch
 import numpy as np
 
-EPOCHS = 3000
+EPOCHS = 10000
 BATCH_SIZE = 6 #16
-LEARNING_RATE = 2 * 1e-4 * np.sqrt(6/16) #downscale learning rate. by multiplying with sqrt(batch_size/original_batch_size) due to reduced batch size
-NUM_BLOCKS = 5 #36
+LEARNING_RATE = 2 * 1e-4
+NUM_BLOCKS = 30 #36
 RES_CHANNELS = 64 #256
 TIMESTEP_LAYER_WIDTH = 512 #512
-TIME_STEPS = 200
-VARIANCE_SCHEDULE = torch.linspace(1e-4, 0.02, TIME_STEPS) #torch.linspace(10e-4, 0.05, TIME_STEPS)
+TIME_STEPS = 50
+VARIANCE_SCHEDULE = torch.linspace(1e-4, 0.05, TIME_STEPS) #torch.linspace(10e-4, 0.05, TIME_STEPS)
 SAMPLE_RATE = 8000 #16000 #22050 #44100
 SAMPLE_LENGTH_SECONDS = 4
 MAX_SAMPLES = 1 #9000 # Use "None" for all samples in data input folder; 1000 = ~1h 6min
