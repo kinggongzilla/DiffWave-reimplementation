fix time embedding broadcast

source/model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 from tqdm import tqdm
 import wandb
-from config import VARIANCE_SCHEDULE, N_MELS, TIME_STEPS, WITH_CONDITIONING, LEARNING_RATE
+from config import VARIANCE_SCHEDULE, N_MELS, TIME_STEPS, WITH_CONDITIONING, LEARNING_RATE, SAMPLE_RATE, SAMPLE_LENGTH_SECONDS
 
 def Conv1d(*args, **kwargs):
   layer = torch.nn.Conv1d(*args, **kwargs)
@@ -93,7 +93,7 @@ class DiffWaveBlock(torch.nn.Module):
         input = x.clone()
         t = self.fc_timestep(t)
         t = t.unsqueeze(-1) # add another dimension at the end
-        t = t.expand(1, 64, 110250) # expand the last dimension to match x; 22050 * 5 = 110250
+        t = t.expand(1, 64, SAMPLE_RATE * SAMPLE_LENGTH_SECONDS) # expand the last dimension to match x; 22500 * 5 = 110250
         x = x + t #broadcast addition
         x = self.conv_dilated(x)
 